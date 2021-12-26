d7b7894a9ad5f14cab3aa2d04a7c0a4cdf47423b
Abhinay Yadav <abhinayy0@gmail.com>
2021-12-27 00:22:34 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+
+class App extends React.Component {
+  state = { quote: "" };
+
+  componentDidMount() {
+    this.fetchQuote();
+  }
+
+  fetchQuote = () => {
+    axios
+      .get(`https://api.adviceslip.com/advice`)
+      .then((res) => {
+        const quote = res.data.slip.advice;
+        this.setState({ quote });
+      })
+      .catch((err) => {});
+  };
+
+  render() {
+    const { quote } = this.state;
+    return <h1>{quote}</h1>;
+  }
 }
 
 export default App;
